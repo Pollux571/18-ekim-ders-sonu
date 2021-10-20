9ff8fbf0ea5b369c3c853bf6906da0ef38c218a8
Pollux571 <88760754+Pollux571@users.noreply.github.com>
2021-10-20 18:23:28 +0300

    first commit

---
+++ javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+let konutKredileri=["Konut Kredisi" , "Emlak konut kredisi", "Kamu konut kredisi" ,"Araba konut kredisi", "Vakif konut kredisi"];
+console.log("<ul>")
+
+
+for (let i = 0; i<konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
